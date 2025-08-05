e6f9f87dac730aa03cdefb41166c8bd32add1411
owenwang802 <yunl34646@gmail.com>
2025-08-05 22:47:41 +0800

    test2 - 12123123

--- src/main/java/com/owen/demo2/Daa.java
+++ src/main/java/com/owen/demo2/Daa.java
@@ -3,7 +3,7 @@ package com.owen.demo2;
 public class Daa {
     public static void main(String[] args) {
         System.out.println("<UNK>");
-        System.out.println("<UNK2>");
+        System.out.println("<UNwwK2>");
         System.out.println("<UNK3>");
         System.out.println("<UNK2>");System.out.println("<UNK2>");
 
