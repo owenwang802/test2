b93152c75caa740375695ff12dbb53280d45e58e
owenwang802 <yunl34646@gmail.com>
2025-08-05 22:42:39 +0800

    test3

--- src/main/java/com/owen/demo2/Daa.java
+++ src/main/java/com/owen/demo2/Daa.java
@@ -4,5 +4,7 @@ public class Daa {
     public static void main(String[] args) {
         System.out.println("<UNK>");
         System.out.println("<UNK2>");
+        System.out.println("<UNKˇ>");
+
     }
 }
